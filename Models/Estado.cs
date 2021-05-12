bca414f50f3a653b69259c87bcb2d88570e10b3d
Lucas Eduardo Bassani da Silva <lucasbassani15@yahoo.com>
2021-05-12 10:30:21 -0300

    Models do projeto criadas

---
+++ Models/Estado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_TURISMO.Models
+{
+    [Table("Estado")]
+    public class Estado
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Eid { get; set; }
+
+
+        [MaxLength(2)]
+        public string uf { get; set; }
+
+        [MaxLength(50)]
+        public string name { get; set; }
+
+    }
+}
---
+++ Models/P_Turistico.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_TURISMO.Models
+{
+    public class P_Turistico
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Nome { get; set; }
+
+        [Column("Descrição")]
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(100, ErrorMessage = "Este campo deve conter entre 3 e 100 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 100 caracteres")]
+        public string Descrição { get; set; }
+
+        [Column("Endereço")]
+        [Display(Name = "Endereço")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Endereço { get; set; }
+
+        [Column("Estado")]
+        [Display(Name = "Estado")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public int EstadoId { get; set; }
+        public Estado Estado { get; set; }
+
+        [Column("Cidade")]
+        [Display(Name = "Cidade")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public int CidadeId { get; set; }
+        public Cidade Cidade { get; set; }
+    }
+}
